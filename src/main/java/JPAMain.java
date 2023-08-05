a2825053c6f091ef11ae0cee2c6876d2dfcab06f
subinjang <eldnj521@gmail.com>
2023-08-06 03:06:46 +0900

    commit

--- src/main/java/JPAMain.java
+++ src/main/java/JPAMain.java
@@ -1,11 +1,9 @@
-package hellojpa;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class JPAMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -13,13 +11,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAA");
 
-            em.clear();
-
-            Member member2 = em.find(Member.class, 150L);
-            System.out.println("==============");
             tx.commit();
         }catch(Exception e){
             tx.rollback();
---
+++ src/main/java/hellojpa/domain/OrderItem.java
@@ -0,0 +1,62 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
